c1a02a410ea7685a239d6e1f85bfecd19244973b
JDGiovanni <146672729+JDGiovanni@users.noreply.github.com>
2026-05-12 18:18:50 -0400

    Add program to search grades by subject
    Implement a simple program to search for grades by subject using a HashMap.

---
+++ LibreriaDeSoporte/src/map.rs
@@ -0,0 +1,23 @@
+use std::collections::HashMap;
+use std::io;
+
+pub fn main() {
+    let mut notas = HashMap::new();
+    notas.insert("Programacion Visual", 20);
+    notas.insert("Diseño de Compiladores", 15);
+    notas.insert("Sistemas Operativos", 12);
+
+    println!("Materia a buscar: ");
+
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Error");
+
+    let input1 = input.trim();
+
+    match notas.get(input1) {
+        Some(nota) => println!("{} {}", input1, nota),
+        None => println!("No existe esa materia"),
+    }
+}
