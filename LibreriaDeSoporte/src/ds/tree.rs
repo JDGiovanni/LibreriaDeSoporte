6974d46be817dee4a0b4ae800e0c2e94aed9544c
JDGiovanni <146672729+JDGiovanni@users.noreply.github.com>
2026-05-12 18:19:32 -0400

    Implement tree structure and interactive sum manager

---
+++ LibreriaDeSoporte/src/ds/tree.rs
@@ -0,0 +1,72 @@
+use std::io;
+#[derive(Debug)]
+pub enum NodoArbol {
+    Numero(i32),
+    Suma(Box<NodoArbol>, Box<NodoArbol>),
+}
+
+impl NodoArbol {
+
+    pub fn nuevo_numero(valor: i32) -> Box<Self> {
+        Box::new(NodoArbol::Numero(valor))
+    }
+
+    pub fn nueva_suma(izq: Box<NodoArbol>, der: Box<NodoArbol>) -> Box<Self> {
+        Box::new(NodoArbol::Suma(izq, der))
+    }
+}
+
+pub fn main () {
+    let mut arbol = NodoArbol::nuevo_numero(0);
+
+    loop {
+        println!("\n===Gestor de Árbol de Sumas ===");
+        println!("1. Nuevo Número (Reiniciar)");
+        println!("2. Sumar al Árbol (Árbol Actual + Nuevo Número)");
+        println!("3. Ver Estructura");
+        println!("4. Salir");
+
+        let mut opcion = String::new();
+        io::stdin()
+            .read_line(&mut opcion)
+            .unwrap();
+        let opcion = opcion.trim();
+
+        match opcion {
+            "1" => {
+                println!("Introduce el nuevo número:");
+                let mut val = String::new();
+                io::stdin()
+                    .read_line(&mut val)
+                    .unwrap();
+                if let Ok(n) = val.trim().parse::<i32>() {
+
+                    arbol = NodoArbol::nuevo_numero(n);
+                    println!("Árbol reiniciado con {}", n);
+                }
+            }
+            "2" => {
+                println!("Introduce el número para sumar:");
+                let mut val = String::new();
+                io::stdin()
+                    .read_line(&mut val)
+                    .unwrap();
+
+                if let Ok(n) = val.trim().parse::<i32>() {
+
+                    let nuevo_nodo = NodoArbol::nuevo_numero(n);
+
+                    arbol = NodoArbol::nueva_suma(arbol, nuevo_nodo);
+                    println!("Suma añadida.");
+                }
+            }
+            "3" => {
+                println!("\nEstructura actual:");
+
+                println!("{:?}", arbol);
+            }
+            "4" => break,
+            _ => println!("Opción no válida"),
+        }
+    }
+}
