499aeb9c2f03e685553aecc8ad035acd3dc2d707
Jhonmar Morales <jhonmar.morales.jr@gmail.com>
2026-05-18 23:28:19 -0400

    Mejora de Main para timestamps

--- LibreriaDeSoporte/src/main.rs
+++ LibreriaDeSoporte/src/main.rs
@@ -3,16 +3,18 @@ use helpers::logger::{Logger, LogLevel};
 use helpers::file_io::FileHelper;
 
 fn main() {
-    Logger::log(LogLevel::Info, "Iniciando pruebas de helpers...");
-    Logger::log(LogLevel::Warning, "Esto es un aviso.");
+    let logger = Logger::new();
+
+    logger.log(LogLevel::Info, "Iniciando pruebas de helpers...");
+    logger.log(LogLevel::Warning, "Esto es un aviso.");
     
     let test_content = "Datos de prueba para la libreria";
     if let Ok(_) = FileHelper::write_file("prueba.txt", test_content) {
-        Logger::log(LogLevel::Info, "Archivo escrito correctamente.");
+        logger.log(LogLevel::Info, "Archivo escrito correctamente.");
     }
 
     match FileHelper::read_file("prueba.txt") {
-        Ok(data) => println!("Contenido leido (Smart Pointer): {}", data),
-        Err(_) => Logger::log(LogLevel::Error, "No se pudo leer el archivo."),
+        Ok(data) => println!("Contenido leido: {}", data),
+        Err(_) => logger.log(LogLevel::Error, "No se pudo leer el archivo."),
     }
 }
